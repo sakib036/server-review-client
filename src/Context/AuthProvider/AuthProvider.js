0f2c8242a5c955a03f8e5d53d8cf601b391d9d18
Md. Nazmus Sakib <sakib036@gmail.com>
2022-11-08 03:31:25 +0600

    footer

--- src/Context/AuthProvider/AuthProvider.js
+++ src/Context/AuthProvider/AuthProvider.js
@@ -11,7 +11,7 @@ const auth=getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser]=useState(null)
-    const [loading,setLoading]=useState(true);
+    const [loading]=useState(true);
 
 
     const createUser=(email,password)=>{
